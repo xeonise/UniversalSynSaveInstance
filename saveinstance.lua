1fd7116eae1f4ee80a313049a7d663d113332b36
xeonise <ghkkq@outlook.com>
2025-04-11 20:46:53 +0300

    Update saveinstance.lua

--- saveinstance.lua
+++ saveinstance.lua
@@ -1943,7 +1943,19 @@ local function synsaveinstance(CustomOptions, CustomOptions2)
 	end
 
 	do
-		local Decompiler = OPTIONS.decomptype == "custom" and custom_decompiler or decompile or custom_decompiler
+		function medaldecom(script)
+		    httpresponse = request({
+		        Url = "http://127.0.0.1:9002",
+		        Body = base64.encode(getscriptbytecode(script)),  -- The base64 encoded bytecode
+		        Method = "POST",
+		        Headers = {
+		            ["Content-Type"] = "text/plain"
+		        },
+		    })
+		    return httpresponse.Body
+		end
+		getgenv().decompile = medaldecom
+		local Decompiler = OPTIONS.decomptype == "custom" and medaldecom
 
 		-- if Decompiler == custom_decompiler then -- Cope
 		-- 	local key = "DecompileTimeout"
